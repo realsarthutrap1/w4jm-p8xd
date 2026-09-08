799bb6919f4d9a4f934cd6b48e3c02e1f1e91afe
Sarthak Pani <realsarthutrap1@users.noreply.github.com>
2026-09-08 11:15:16 -0700

    lab6: add function stubs

--- main.cpp
+++ main.cpp
@@ -4,8 +4,22 @@ using namespace std;
 
 const int SIZE = 5;
 
+void enterArrayData(double *arr, int size);
+void outputArrayData(double *arr, int size);
+double sumArray(double *arr, int size);
+
 int main() {
     double *arr = new double[SIZE];
 
+    return 0;
+}
+
+void enterArrayData(double *arr, int size) {
+}
+
+void outputArrayData(double *arr, int size) {
+}
+
+double sumArray(double *arr, int size) {
     return 0;
 }
