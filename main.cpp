ccbb9cbbdcfb9f2a1d70697aa7d181a3651721c8
Sarthak Pani <realsarthutrap1@users.noreply.github.com>
2026-09-08 11:45:38 -0700

    lab6: add outputArrayData

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ int main() {
     double *arr = new double[SIZE];
 
     enterArrayData(arr, SIZE);
+    outputArrayData(arr, SIZE);
 
     return 0;
 }
@@ -26,6 +27,11 @@ void enterArrayData(double *arr, int size) {
 }
 
 void outputArrayData(double *arr, int size) {
+    cout << "Outputting array elements: ";
+    for (int i = 0; i < size; i++) {
+        cout << *(arr + i) << " ";
+    }
+    cout << endl;
 }
 
 double sumArray(double *arr, int size) {
